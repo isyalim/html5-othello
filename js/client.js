afb3e33615c7e1c55b9024af3f1d71c2008c35c2
brian.clanton <brian.clanton@49672d30-05e5-623a-be9b-53f7106c8c5d>
2012-11-05 23:11:23 +0000

    Started working on cacheImages method of client.js.

--- js/client.js
+++ js/client.js
@@ -3,6 +3,8 @@ var game = Othello.namespace("client.game");
 game = function() {
 	// Constants
 	var EMPTY = 0, WHITE = 1, BLACK = 2;
+	var NUM_ROWS = 8, NUM_COLS = 8;
+	var GRID_SIZE = 50;
 
 	// Variables
 	var boardImage;
@@ -22,14 +24,30 @@ game = function() {
 		var tempCanvas, tempCtx;
 
 		// Draw board
+		var boardWidth = NUM_COLS * GRID_SIZE, boardHeight = NUM_ROWS * GRID_SIZE;
 		tempCanvas = document.createElement("canvas");
-		tempCanvas.width = 400;
-		tempCanvas.height = 400;
+		tempCanvas.width = boardWidth;
+		tempCanvas.height = boardHeight;
 		tempCtx = tempCanvas.getCtx("2d");
 
 		// Green board
 		tempCtx.fillStyle = "#4C9E00";
-		tempCtx.fillRect(0, 0, 150, 175);
+		tempCtx.fillRect(0, 0, boardWidth, boardHeight);
+
+		// Grid lines
+		for (var i = 1; i < NUM_ROWS - 1; i++) {
+			for (var j = 1; j < NUM_COLS - 1; j++) {
+				ctx.moveTo(i * GRID_SIZE, 0);
+				ctx.lineTo(i * GRID_SIZE, boardHeight);
+				ctx.stroke();
+			}
+
+			ctx.moveTo(0, j * GRID_SIZE);
+			ctx.lineTo(boardWidth, j * GRID_SIZE);
+			ctx.stroke();
+		}
+
+		
 
 		// Draw white piece
 
