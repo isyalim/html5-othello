58e503ba38bcf0b60245bdb7d7260a956f75b998
brian.clanton <brian.clanton@49672d30-05e5-623a-be9b-53f7106c8c5d>
2012-11-07 23:36:30 +0000

    Completed merge from my branch.

--- trunk/js/client.js
+++ trunk/js/client.js
@@ -1,6 +1,4 @@
-var game = Othello.namespace("client.game");
-
-game = function() {
+Othello.client.game = function() {
 	// Constants
 	var EMPTY = 0, WHITE = 1, BLACK = 2;
 	var NUM_ROWS = 8, NUM_COLS = 8;
@@ -16,6 +14,8 @@ game = function() {
 	init = function() {
 		canvas = document.querySelector("#gameBoard");
 		ctx = canvas.getContext("2d");
+
+		cacheImages();
 	}
 
 
@@ -28,27 +28,28 @@ game = function() {
 		tempCanvas = document.createElement("canvas");
 		tempCanvas.width = boardWidth;
 		tempCanvas.height = boardHeight;
-		tempCtx = tempCanvas.getCtx("2d");
+		tempCtx = tempCanvas.getContext("2d");
 
 		// Green board
 		tempCtx.fillStyle = "#4C9E00";
 		tempCtx.fillRect(0, 0, boardWidth, boardHeight);
 
 		// Grid lines
-		for (var i = 1; i < NUM_ROWS - 1; i++) {
-			for (var j = 1; j < NUM_COLS - 1; j++) {
-				ctx.moveTo(i * GRID_SIZE, 0);
-				ctx.lineTo(i * GRID_SIZE, boardHeight);
-				ctx.stroke();
+		for (var i = 1; i < NUM_ROWS; i++) {
+			for (var j = 1; j < NUM_COLS; j++) {
+				tempCtx.moveTo(j * GRID_SIZE, 0);
+				tempCtx.lineTo(j * GRID_SIZE, boardHeight);
+				tempCtx.stroke();
 			}
 
-			ctx.moveTo(0, j * GRID_SIZE);
-			ctx.lineTo(boardWidth, j * GRID_SIZE);
-			ctx.stroke();
+			tempCtx.moveTo(0, i * GRID_SIZE);
+			tempCtx.lineTo(boardWidth, i * GRID_SIZE);
+			tempCtx.stroke();
 		}
 
+		boardImage = new Image();
+		boardImage.src = tempCanvas.toDataURL();
 		
-
 		// Draw white piece
 
 		// Draw black piece
@@ -57,6 +58,7 @@ game = function() {
 	// Draws game board and pieces
 	draw = function() {
 		// Draw board image to screen
+		ctx.drawImage(boardImage, 0, 0);
 
 		// Draw pieces based on current board state
 	}
@@ -74,6 +76,7 @@ game = function() {
 	}
 
 	return {
-		draw : draw
+		draw : draw,
+		init : init
 	}
 }();
