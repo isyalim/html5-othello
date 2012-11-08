68784d9a13bf93a62a5b13156133bc1100bc2594
ajh2510@g.rit.edu <ajh2510@g.rit.edu@49672d30-05e5-623a-be9b-53f7106c8c5d>
2012-11-08 19:12:40 +0000

    modidifed loader.js

--- trunk/js/loader.js
+++ trunk/js/loader.js
@@ -14,16 +14,24 @@ window.addEventListener("load", function() {
 	Modernizr.load([{
 		load : [
 			// Libraries
-			
 			// Scripts
 			/*"js/utils.js"*/, "js/client.js"
 		],
 
 		complete : function() {
 			console.log("All files have been loaded.", Othello);
-			
+			console.log("load");
+			document.getElementById("mainscreen").style.visibility = "hidden";
+			document.getElementById("gamescreen").style.visibility="hidden";
+			document.getElementById("loginbutton").addEventListener("click", function(){
+			document.getElementById("login").style.visibility = "hidden";
+			document.getElementById("gamescreen").style.visibility = "visible";
+
+			});
 			Othello.client.game.init();
 			Othello.client.game.draw();
+			console.log("load");
+			
 		}
 	}]);
 }, false);
