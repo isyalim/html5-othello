adb686edfa849290a74dfd5cf8448a534ed4f493
ajh2510 <ajh2510@g.rit.edu>
2012-11-13 04:17:32 +0000

    Added entering the actual game, as well as the ability to make moves and have the pieces change color correctly.(turns haven't been implemented yet).

--- js/othelloserver.js
+++ js/othelloserver.js
@@ -3,6 +3,7 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 var playerList= new Array();
 var boardList= new Array();
+var gameIDCounter=0;
 
 
 var server = http.createServer(function(request, response) {
@@ -44,10 +45,10 @@ wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
             console.log('Received Message: ' + message.utf8Data);
-			var recievedData=eval('(' + message.utf8Data + ')');
-			switch(recievedData.keyCode){
+			var receivedData=eval('(' + message.utf8Data + ')');
+			switch(receivedData.keyCode){
 				case 0:
-					var playerName=recievedData.loginData;
+					var playerName=receivedData.loginData;
 					var playerExists=false;
 					for(var i=0; i<playerList.length;i++)
 					{
@@ -82,8 +83,8 @@ wsServer.on('request', function(request) {
 				break;
 				  
 				  case 5:
-				  var chatData= recievedData.chatData;
-				  var sentFrom= recievedData.sentFrom;
+				  var chatData= receivedData.chatData;
+				  var sentFrom= receivedData.sentFrom;
 				  var now= new Date();
 				  
 				  var sentMessage = sentFrom + "(" +  (now.getHours() +1) +  ":" +now.getMinutes() +  ":" +now.getSeconds() + "): " + chatData;
@@ -101,8 +102,8 @@ wsServer.on('request', function(request) {
 				  break;
 				  
 				  case 7:
-				  var inviteTo=recievedData.inviteTo;
-				  var invitefrom=recievedData.inviteFrom;
+				  var inviteTo=receivedData.inviteTo;
+				  var invitefrom=receivedData.inviteFrom;
 				  for(var i=0; i<playerList.length;i++)
 				  {
 					var p= playerList[i];
@@ -115,6 +116,86 @@ wsServer.on('request', function(request) {
 						p.connection.send(JSON.stringify(o));
 					}
 				  }
+				  break;
+				  
+				  case 9:
+					var sentFrom=receivedData.sentFrom;
+					var acceptedPlayer=receivedData.acceptedPlayer;
+					var playerToFind="";
+					var sentFromPlayer="";
+					var toFindIndex=-1
+					var sentFromIndex=-1;
+					for(var i=0; i<playerList.length;i++){
+						var p=playerList[i];
+						if(p.name==acceptedPlayer)
+						{
+							playerToFind=p;
+							toFindIndex=i;
+						}
+						else if(p.name==sentFrom)
+						{
+							sentFromPlayer=p;
+							sentFromIndex=i;
+						}
+					}
+					//both accepted
+					if(playerToFind!="" && !playerToFind.inGame){
+						gameIDCounter++;
+						var createdGame=new GameBoard(new Array([0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,1,2,0,0,0],[0,0,0,2,1,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]),gameIDCounter,sentFromPlayer.name,playerToFind.name);
+						var o={
+							keyCode:11,
+							newGame:createdGame
+						}
+						playerList[toFindIndex].inGame=true;
+						playerList[toFindIndex].gameID=gameIDCounter;
+						playerList[sentFromIndex].inGame=true;
+						playerList[sentFromIndex].gameID=gameIDCounter;
+						sentFromPlayer.connection.send(JSON.stringify(o));
+						playerToFind.connection.send(JSON.stringify(o));
+					}
+					//accepted, but other player not found
+					else if(playerToFind==""){
+						var o={
+							keyCode:12,
+							errorMsg:"Player " + acceptedPlayer+ " not found."
+						}
+						sentFromPlayer.connection.send(JSON.stringify(o));
+					}
+					//accepted, but other player already in game
+					else if(playerToFind.inGame){
+						var o={
+							keyCode:12,
+							errorMsg:"Player " + acceptedPlayer +" is already in game."
+						}
+						sentFromPlayer.connection.send(JSON.stringify(o));
+					}
+				  break;
+				  case 10:
+					
+					var sentFrom=receivedData.sentFrom;
+					var declinedPlayer=receivedData.declinedPlayer;
+					var playerToFind="";
+					for(var i=0; i<playerList.length;i++){
+						var p=playerList[i];
+						if(p.name==declinedPlayer)
+						{
+							
+							playerToFind=p;
+							console.log(p.name + " " + playerToFind);
+						}
+					}
+					if(playerToFind!=""){
+						var o={
+							keyCode:12,
+							errorMsg:"Player " + sentFrom +" has declined your invitation."
+						}
+						console.log("sent 12");
+						playerToFind.connection.send(JSON.stringify(o));
+					}
+					else
+					{
+						console.log("breaks");
+					}
 				  break;
 			}
 			
@@ -153,7 +234,7 @@ function currentPlayers()
 	var returnedArray= new Array();
 	for(var i=0; i<playerList.length;i++)
 	{
-		if(playerList[i].gameId==0)
+		if(!playerList[i].inGame)
 		{
 			returnedArray.push(playerList[i].name);
 		}
@@ -163,17 +244,14 @@ function currentPlayers()
 }
 
 
-
-
 //ITEMS
 function GameBoard(curBoard,gID,p1,p2){
 				//ivars - unique for every instance
 				this.layout=curBoard;
-				this.gameId=gameId;
+				this.gameId=gID;
 				this.player1=p1;
 				this.player2=p2;
 				this.whoseTurn=this.player1;
-				return this;
 			}
 function ClientPlayer(pName,conn){
 				//ivars - unique for every instance
